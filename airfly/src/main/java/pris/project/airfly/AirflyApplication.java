00d8b4e3c67e323e515ce403b6f2b7aa2c1f5191
Gita <aleksandar.gitaric@pmf.uns.ac.rs>
2021-05-05 05:24:41 +0200

    new project structure

---
+++ airfly/src/main/java/pris/project/airfly/AirflyApplication.java
@@ -0,0 +1,13 @@
+package pris.project.airfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirflyApplication.class, args);
+	}
+
+}
---
+++ airfly/src/test/java/pris/project/airfly/AirflyApplicationTests.java
@@ -0,0 +1,13 @@
+package pris.project.airfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
